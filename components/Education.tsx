74e1fca669e4861e17daf1bd2816b5d6941be2d0
Vandyck <vandycklai@gmail.com>
2024-08-10 15:49:12 +0800

    Set typescript

--- components/Education.tsx
+++ components/Education.tsx
@@ -74,7 +74,7 @@ export default function Education() {
   )
 }
 
-function ResourceCard({ title, level, type }) {
+function ResourceCard({ title, level, type }: {title: string, level: string, type: string}) {
   return (
     <div className="bg-white rounded-lg shadow-md p-6">
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
